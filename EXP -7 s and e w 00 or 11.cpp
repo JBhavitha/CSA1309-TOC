83b7564bdf03289e7b3c8e1d6c3733262db98dc4
JBhavitha <113425167+JBhavitha@users.noreply.github.com>
2023-11-19 16:27:32 +0530

    Add files via upload

---
+++ EXP -7 s and e w 00 or 11.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char string[100];
+	int l;
+	printf("enter the string : ");
+	scanf("%s",string);
+	l=strlen(string);
+	if(string[0]=='0'||string[0]=='1' && string[l-1]=='1' || string[l-1]=='0')
+	{
+		for( int i=0 ; i<l;i++)
+		{
+			if(string[i]>'1')
+			{
+				printf("invalid");
+				return 0;
+			}
+		}
+		printf("accepted ");
+	}
+	else 
+	{
+		printf("not accepted ");
+	}
+}
